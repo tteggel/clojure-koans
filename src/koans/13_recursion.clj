fb1a4a66cb18c6c392bfeb06c067af9d712aa17c
Thom Leggett <github@thomleggett.co.uk>
2012-08-01 11:39:42 +0100

    01-13 completed at BrisFunctional dojo-style 2012-07-31

--- src/koans/13_recursion.clj
+++ src/koans/13_recursion.clj
@@ -1,20 +1,27 @@
 (defn is-even? [n]
   (if (= n 0)
-    __
-    (___ (is-even? (dec n)))))
+    true
+    (not (is-even? (dec n)))))
 
 (defn is-even-bigint? [n]
   (loop [n   n
          acc true]
     (if (= n 0)
-      __
+      acc
       (recur (dec n) (not acc)))))
 
-(defn recursive-reverse [coll]
-  __)
+(defn recursive-reverse
+  ([coll] (recursive-reverse coll []))
+  ([coll acc] (if (empty? coll)
+                acc
+                (recur (rest coll) (cons (first coll) acc)))))
 
 (defn factorial [n]
-  __)
+  (loop [n   n
+         acc 1]
+    (if (= n 0)
+      acc
+      (recur (dec n) (* n acc)))))
 
 (meditations
   "Recursion ends with a base case"
